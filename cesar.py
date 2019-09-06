1c9d858a689d2a5b5bf14ad3e0cc638baedcf2f3
Marcos Henrique Curtale Serafim <Marcos.Henrique@schumann.com.br>
2019-09-06 16:09:33 -0300

    Implemented Cesar Cypher

---
+++ cesar.py
@@ -0,0 +1,24 @@
+crypted_msg = ''
+descrypted_msg = ''
+
+print('Mensagem para cifrar: ')
+message = [c for c in input()]
+
+print('Chave: ')
+key = ord(input()[0])
+
+for i in message:
+    if(ord(i) > 255):
+        raise Exception('Invalid input. Character {} does not belong to the alphabet'.format(i))
+
+print('Mensagem criptografada: ')
+for i in message:
+    crypted_msg += chr((ord(i) + key) % 255)
+
+print(crypted_msg)
+
+print('Mensagem descriptografada: ')
+for i in crypted_msg:
+    descrypted_msg += chr((ord(i) - key) % 255)
+
+print(descrypted_msg)
