a06efa41a555eb14a9ac336e2dbc9dea1c74bfcf
Marcos Henrique Curtale Serafim <Marcos.Henrique@schumann.com.br>
2019-09-23 13:27:33 -0300

    Char frequency. Duo_char frequency calculations.

--- cifra_substituicao/char_frequency.py
+++ cifra_substituicao/char_frequency.py
@@ -1,4 +1,7 @@
-frequencies = [0 for i in range(255)]
+char_frequencies = [0 for i in range(255)]
+duo_freq = {}
+duo_frequencies = []
+tri_freq = {}
 lenght=0
 
 with open("texto_base.txt") as f:
@@ -8,10 +11,27 @@ with open("texto_base.txt") as f:
       break
     if ord(c) < 255:
         lenght += 1
-        frequencies[ord(c)] += 1
+        char_frequencies[ord(c)] += 1
 
-frequencies = [i / lenght*100 for i in frequencies]
+char_frequencies = [{"char":i, "freq": c / lenght*100} for i, c in enumerate(char_frequencies, start=0)]
 
 # print('Alphabetic frequencies:')
 # for i in 'abcdefghijklmnopqrstuvwxyz':
-#     print(i + ': ' + str(frequencies[ord(i)]*100))
+#     print(i + ': ' + str(char_frequencies[ord(i)]*100))
+
+# print(sorted(char_frequencies, key = lambda i: i['freq'], reverse = True) )
+duo_count = 0
+for line in open("texto_base2.txt").readlines():
+  for i, char in enumerate(line, start=0):
+    if i == len(line)-1:
+      break
+    if char+line[i+1] not in duo_freq:
+      duo_freq[char+line[i+1]] = 0
+    else:
+      duo_freq[char+line[i+1]] += 1
+    duo_count += 1
+
+for el in duo_freq:
+  duo_frequencies.append({"duo":el, "freq": duo_freq[el] / duo_count * 100 })
+
+duo_frequencies = sorted(duo_frequencies, key = lambda i: i['freq'], reverse = True)
--- cifra_substituicao/cifra_substituicao.py
+++ cifra_substituicao/cifra_substituicao.py
@@ -2,25 +2,46 @@
 
 crypted_msg = []
 descrypted_msg = ''
-from char_frequency import frequencies
+from char_frequency import char_frequencies, duo_frequencies
 
 def random_char():
-
     return [i for i in reversed(range(255))]
 
 key = random_char()
 
-print('Mensagem para cifrar: ')
-message = [c for c in input()]
+# print('Mensagem para cifrar: ')
+message = [c for c in 'Era uma vez em Hollywood. Filme de Quentin Tarantino. Teste mensagem criptografada']
+# message = [c for c in 'aeiouAEIOU']
 
 for i in message:
     crypted_msg.append(key[ord(i)])
 
-print(crypted_msg)
+# print('Mensagem descriptografada: ')
+# for i in crypted_msg:
+#     descrypted_msg += chr(key[int(i)])
+
+# print(descrypted_msg)
+
+crypted_frequencies = [0 for i in range(255)]
+
+for c in crypted_msg:
+    crypted_frequencies[c] += 1
+
+crypted_frequencies = [{"char":i, "freq": c / len(crypted_msg)*100} for i, c in enumerate(crypted_frequencies, start=0)]
+key_attempt = [-1 for i in range(255)]
+
+sorted_freqs = sorted(char_frequencies, key = lambda i: i['freq'], reverse = True)
+
+for i, el in enumerate(sorted(crypted_frequencies, key = lambda i: i['freq'], reverse = True), start = 0):
+    key_attempt[el['char']] = sorted_freqs[i]['char']
 
 print('Mensagem descriptografada: ')
 for i in crypted_msg:
     descrypted_msg += chr(key[int(i)])
+print(descrypted_msg)
+descrypted_msg = ''
+for i in crypted_msg:
+    # print(key_attempt[int(i)])
+    descrypted_msg += chr(key_attempt[int(i)])
 
 print(descrypted_msg)
-print(frequencies)
