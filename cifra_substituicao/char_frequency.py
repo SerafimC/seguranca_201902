24d0ec7331d4bdee7615ba155ff1c8186fd10789
Marcos Henrique Curtale Serafim <Marcos.Henrique@schumann.com.br>
2019-09-24 16:33:51 -0300

    Clening up the code

--- cifra_substituicao/char_frequency.py
+++ cifra_substituicao/char_frequency.py
@@ -1,37 +1,54 @@
-char_frequencies = [0 for i in range(255)]
-duo_freq = {}
-duo_frequencies = []
-tri_freq = {}
-lenght=0
+filename = "texto_base2.txt"
 
-with open("texto_base.txt") as f:
-  while True:
-    c = f.read(1)
-    if not c:
-      break
-    if ord(c) < 255:
-        lenght += 1
-        char_frequencies[ord(c)] += 1
+def get_frequencies(filename):
+  char_frequencies = [0 for i in range(255)]
+  duo_freq = tri_freq =  {}
+  duo_frequencies = trio_frequencies = []
+  lenght=0
 
-char_frequencies = [{"char":i, "freq": c / lenght*100} for i, c in enumerate(char_frequencies, start=0)]
+  with open(filename) as f:
+    while True:
+      c = f.read(1)
+      if not c:
+        break
+      if ord(c) < 255:
+          lenght += 1
+          char_frequencies[ord(c)] += 1
 
-# print('Alphabetic frequencies:')
-# for i in 'abcdefghijklmnopqrstuvwxyz':
-#     print(i + ': ' + str(char_frequencies[ord(i)]*100))
+  char_frequencies = [{"char":i, "freq": c / lenght*100} for i, c in enumerate(char_frequencies, start=0)]
 
-# print(sorted(char_frequencies, key = lambda i: i['freq'], reverse = True) )
-duo_count = 0
-for line in open("texto_base2.txt").readlines():
-  for i, char in enumerate(line, start=0):
-    if i == len(line)-1:
-      break
-    if char+line[i+1] not in duo_freq:
-      duo_freq[char+line[i+1]] = 0
-    else:
-      duo_freq[char+line[i+1]] += 1
-    duo_count += 1
+  count = 0
+  for line in open(filename).readlines():
+    for i, char in enumerate(line, start=0):
+      if i == len(line)-1:
+        break
+      if char+line[i+1] not in duo_freq:
+        duo_freq[char+line[i+1]] = 0
+      else:
+        duo_freq[char+line[i+1]] += 1
+      count += 1
 
-for el in duo_freq:
-  duo_frequencies.append({"duo":el, "freq": duo_freq[el] / duo_count * 100 })
+  for el in duo_freq:
+    duo_frequencies.append({"duo":el, "freq": duo_freq[el] / count * 100 })
 
-duo_frequencies = sorted(duo_frequencies, key = lambda i: i['freq'], reverse = True)
+  duo_frequencies = sorted(duo_frequencies, key = lambda i: i['freq'], reverse = True)
+
+  count = 0
+  for line in open(filename).readlines():
+    for i, char in enumerate(line, start=0):
+      if i >= len(line)-2:
+        break
+      if char+line[i+1]+line[i+2] not in tri_freq:
+        tri_freq[char+line[i+1]+line[i+2]] = 0
+      else:
+        tri_freq[char+line[i+1]+line[i+2]] += 1
+      count += 1
+
+  for el in tri_freq:
+    trio_frequencies.append({"trio":el, "freq": tri_freq[el] / count * 100 })
+
+  trio_frequencies = sorted(trio_frequencies, key = lambda i: i['freq'], reverse = True)
+
+  return char_frequencies, duo_frequencies, trio_frequencies
+
+char_frequencies, duo_frequencies, trio_frequencies = get_frequencies(filename)
--- cifra_substituicao/cifra_substituicao.py
+++ cifra_substituicao/cifra_substituicao.py
@@ -1,47 +1,94 @@
-from char_frequency import char_frequencies, duo_frequencies
+from char_frequency import char_frequencies, duo_frequencies, trio_frequencies
 # https://www.gta.ufrj.br/ensino/eel879/trabalhos_vf_2010_2/gabriel/cript.htm
 
-crypted_msg = []
-descrypted_msg = ''
-
-def random_char():
+def random_key():
     return [i for i in reversed(range(255))]
 
-key = random_char()
+def decrypt_message(crypted_msg, key):
+    descrypted_msg = ''
+    
+    for i in crypted_msg:
+        descrypted_msg += chr(key[int(i)])
+    return descrypted_msg
 
-# print('Mensagem para cifrar: ')
-message = [c for c in 'Era uma vez em Hollywood. Filme de Quentin Tarantino. Teste mensagem criptografada']
-# message = [c for c in 'aeiouAEIOU']
+def crypt_message(message, key):
+    crypted_msg = []
+    for i in message:
+        crypted_msg.append(key[ord(i)])
+    return crypted_msg
 
-for i in message:
-    crypted_msg.append(key[ord(i)])
+def getCharFreqs(crypted_msg):
+    crypted_frequencies = [0 for i in range(255)]
 
-# print('Mensagem descriptografada: ')
-# for i in crypted_msg:
-#     descrypted_msg += chr(key[int(i)])
+    for c in crypted_msg:
+        crypted_frequencies[c] += 1
 
-# print(descrypted_msg)
+    return [{"char":i, "freq": c / len(crypted_msg)*100} for i, c in enumerate(crypted_frequencies, start=0)]
 
-crypted_frequencies = [0 for i in range(255)]
+def getDuoFreqs(crypted_msg):
+    duo_dict = {}
+    duo_frequencies = []
+    count = 0
 
-for c in crypted_msg:
-    crypted_frequencies[c] += 1
+    for i, char in enumerate(crypted_msg, start=0):
+        if i >= len(crypted_msg)-1:
+            break
+        el = str(char)+'|'+str(crypted_msg[i+1])
+        if el not in duo_dict:
+            duo_dict[el] = 0
+        else:
+            duo_dict[el] += 1
+        count += 1
 
-crypted_frequencies = [{"char":i, "freq": c / len(crypted_msg)*100} for i, c in enumerate(crypted_frequencies, start=0)]
-key_attempt = [-1 for i in range(255)]
+    for el in duo_dict:
+        duo_frequencies.append({"duo":el, "freq": duo_dict[el] / count * 100 })
 
-sorted_freqs = sorted(char_frequencies, key = lambda i: i['freq'], reverse = True)
+    return sorted(duo_frequencies, key = lambda i: i['freq'], reverse = True)
 
-for i, el in enumerate(sorted(crypted_frequencies, key = lambda i: i['freq'], reverse = True), start = 0):
-    key_attempt[el['char']] = sorted_freqs[i]['char']
+def getTrioFreqs(crypted_msg):
+    trio_dict = {}
+    trio_frequencies = []
+    count = 0
 
-print('Mensagem descriptografada: ')
-for i in crypted_msg:
-    descrypted_msg += chr(key[int(i)])
-print(descrypted_msg)
-descrypted_msg = ''
-for i in crypted_msg:
-    # print(key_attempt[int(i)])
-    descrypted_msg += chr(key_attempt[int(i)])
+    for i, char in enumerate(crypted_msg, start=0):
+        if i >= len(crypted_msg)-2:
+            break
+        el = str(char)+'|'+str(crypted_msg[i+1])+'|'+str(crypted_msg[i+2])
+        if el not in trio_dict:
+            trio_dict[el] = 0
+        else:
+            trio_dict[el] += 1
+        count += 1
 
-print(descrypted_msg)
+    for el in trio_dict:
+        trio_frequencies.append({"trio":el, "freq": trio_dict[el] / count * 100 })
+
+    return sorted(trio_frequencies, key = lambda i: i['freq'], reverse = True)
+
+def main():
+
+    key = random_key()
+    key_attempt_points = [0 for i in range(255)]
+    message = [c for c in 'Era uma vez em Hollywood. Filme de Quentin Tarantino. Teste mensagem criptografada. Quanto mais texto, mais facil de identificar os padroes.']
+    crypted_msg = crypt_message(message, key)
+
+    crypted_char_frequencies = getCharFreqs(crypted_msg)
+    crypted_duo_frequencies = getDuoFreqs(crypted_msg)
+    crypted_trio_frequencies = getTrioFreqs(crypted_msg)
+
+    key_attempt = [-1 for i in range(255)]
+    sorted_freqs = sorted(char_frequencies, key = lambda i: i['freq'], reverse = True)
+
+    for i, el in enumerate(sorted(crypted_char_frequencies, key = lambda i: i['freq'], reverse = True), start = 0):
+        key_attempt[el['char']] = sorted_freqs[i]['char']
+
+    descrypted_msg = decrypt_message(crypted_msg, key_attempt)
+
+    for i, el in enumerate(key_attempt_points, start=0):
+        for char in crypted_char_frequencies:
+            if char['char'] == i:
+                key_attempt_points[i] += char['freq']
+
+    print(key_attempt_points)
+    
+main()
